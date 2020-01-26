35b77febab94e362ed6145b8ae6f4f410288c0f4
Евгений Сажин <prosazhin@MacBook-Pro-Evgenij.local>
2020-01-26 14:52:05 +0300

    fix code style

--- pages/index.js
+++ pages/index.js
@@ -4,63 +4,63 @@ import Head from '../components/head';
 import Nav from '../components/nav';
 
 const Home = () => {
-  const [date, setDate] = useState(null);
+	const [date, setDate] = useState(null);
 
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.json();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+	useEffect(() => {
+		async function getDate() {
+			const res = await fetch('/api/date');
+			const newDate = await res.json();
+			setDate(newDate);
+		}
+		getDate();
+	}, []);
 
-  return (
-    <div>
-      <Head title="Home" />
-      <Nav />
+	return (
+		<div>
+			<Head title="Home" />
+			<Nav />
 
-      <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
-        <p className="description">
-          To get started, edit the <code>pages/index.js</code> or{' '}
-          <code>pages/api/date.js</code> files, then save to reload.
+			<div className="hero">
+				<h1 className="title">Welcome to Next!</h1>
+				<p className="description">
+					To get started, edit the <code>pages/index.js</code> or{' '}
+					<code>pages/api/date.js</code> files, then save to reload.
         </p>
 
-        <p className="row date">
-          The date is:&nbsp;{' '}
-          {date ? (
-            <span>
-              <b>{date.date}</b>
-            </span>
-          ) : (
-            <span className="loading"></span>
-          )}
-        </p>
+				<p className="row date">
+					The date is:&nbsp;{' '}
+					{date ? (
+						<span>
+							<b>{date.date}</b>
+						</span>
+					) : (
+							<span className="loading"></span>
+						)}
+				</p>
 
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
-        </div>
-      </div>
+				<div className="row">
+					<Link href="https://github.com/zeit/next.js#setup">
+						<a className="card">
+							<h3>Getting Started &rarr;</h3>
+							<p>Learn more about Next.js on GitHub and in their examples.</p>
+						</a>
+					</Link>
+					<Link href="https://github.com/zeit/next.js/tree/master/examples">
+						<a className="card">
+							<h3>Examples &rarr;</h3>
+							<p>Find other example boilerplates on the Next.js GitHub.</p>
+						</a>
+					</Link>
+					<Link href="https://github.com/zeit/next.js">
+						<a className="card">
+							<h3>Create Next App &rarr;</h3>
+							<p>Was this tool helpful? Let us know how we can improve it!</p>
+						</a>
+					</Link>
+				</div>
+			</div>
 
-      <style jsx>{`
+			<style jsx>{`
         .hero {
           width: 100%;
           color: #333;
@@ -136,8 +136,8 @@ const Home = () => {
           color: #333;
         }
       `}</style>
-    </div>
-  );
+		</div>
+	);
 };
 
 export default Home;
