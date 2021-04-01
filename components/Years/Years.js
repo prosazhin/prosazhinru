bc53ee8729ee98dffe0d7c491377078d046a68ea
prosazhin <prosazhin@yandex.ru>
2021-04-01 19:43:52 +0300

    Список достижений на главной

--- components/Years/Years.js
+++ components/Years/Years.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-// import { Links, Tags } from '../'
+import numeralize from 'numeralize-ru'
 import style from './Years.module.scss'
 
 
@@ -8,11 +8,67 @@ import style from './Years.module.scss'
 export default function Years({ array }) {
     return (
         <ul className={style.years}>
-            {array.sort(( a, b ) =>  b.title - a.title).map(item =>
+            {array.sort(( a, b ) =>  b.title - a.title).map((item, index) =>
                 <li className={style.year} key={item.title}>
                     <span className={style.year__headline}>
                         {item.title}
                     </span>
+                    <ul className={style.logs}>
+                        {(!item.job.fired && !item.job.hired && !!item.job.work) &&
+                            <li className={style.log}>
+                                <span className={style.log__title}>
+                                    {index === 0 ?
+                                        'Работаю в'
+                                        :
+                                        'Работал в'
+                                    }
+                                </span>
+                                <span className={style.log__value}>
+                                    {item.job.work.title}
+                                </span>
+                            </li>
+                        }
+                        {item.job.fired &&
+                            <li className={style.log}>
+                                <span className={style.log__title}>Уволился из</span>
+                                <span className={style.log__value}>
+                                    {item.job.fired.title}
+                                </span>
+                            </li>
+                        }
+                        {item.job.hired &&
+                            <li className={style.log}>
+                                <span className={style.log__title}>Устроился в</span>
+                                <span className={style.log__value}>
+                                    {item.job.hired.title}
+                                </span>
+                            </li>
+                        }
+                        {!!item.links.length &&
+                            <li className={style.log}>
+                                <span className={style.log__title}>Добавил</span>
+                                <span className={style.log__value}>
+                                    {item.links.length} {numeralize.pluralize(item.links.length, 'ссылка', 'ссылки', 'ссылок')}
+                                </span>
+                            </li>
+                        }
+                        {!!item.selections.length &&
+                            <li className={style.log}>
+                                <span className={style.log__title}>Составил</span>
+                                <span className={style.log__value}>
+                                    {item.selections.length} {numeralize.pluralize(item.selections.length, 'подборка', 'подборки', 'подборок')}
+                                </span>
+                            </li>
+                        }
+                        {!!item.posts.length &&
+                            <li className={style.log}>
+                                <span className={style.log__title}>Написал</span>
+                                <span className={style.log__value}>
+                                    {item.posts.length} {numeralize.pluralize(item.posts.length, 'заметка', 'заметки', 'заметок')}
+                                </span>
+                            </li>
+                        }
+                    </ul>
                 </li>
             )}
         </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
 
 import {
@@ -12,6 +13,7 @@ import {
 	pagesSerializer,
 	contactsSerializer,
 	jobsSerializer,
+	linksSerializer,
 	selectionsSerializer,
 	postsSerializer,
 } from '../utils/Serializers'
@@ -25,6 +27,7 @@ export async function getStaticProps() {
 	const pagesResult = pagesSerializer(await api.get('pages'), 'home')
 	const contactsResult = contactsSerializer(await api.get('contacts'))
 	const jobsResult = jobsSerializer(await api.get('jobs'))
+	const linksResult = linksSerializer(await api.get('links', { limit: 500, include: 0 }))
 	const selectionsResult = selectionsSerializer(await api.get('selections'))
 	const postsResult = postsSerializer(await api.get('posts'))
 
@@ -34,6 +37,7 @@ export async function getStaticProps() {
 			navigationsList: pagesResult.navigations,
 			contactsList: contactsResult,
 			jobsList: jobsResult,
+			linksList: linksResult,
 			selectionsList: selectionsResult,
 			postsList: postsResult,
 		},
@@ -47,48 +51,58 @@ export default function HomePage({
 	navigationsList,
 	contactsList,
 	jobsList,
+	linksList,
 	selectionsList,
 	postsList,
 }) {
 	const router = useRouter()
 
+	function workNow(item, year) {
+		if (!item.dismissal && dayjs(item.recruited).format('YYYY') < year) {
+			return true
+		}
+
+		if (dayjs(item.dismissal).format('YYYY') > year && dayjs(item.recruited).format('YYYY') < year) {
+			return true
+		}
+
+		return false
+	}
+
 	const yearsList = [
 		{
 			title: 2021,
-			job: null,
-			links: null,
-			selections: null,
-			posts: null,
+			titleString: '2021',
 		},
 		{
 			title: 2020,
-			job: null,
-			links: null,
-			selections: null,
-			posts: null,
+			titleString: '2020',
 		},
 		{
 			title: 2019,
-			job: null,
-			links: null,
-			selections: null,
-			posts: null,
+			titleString: '2019',
 		},
 		{
 			title: 2018,
-			job: null,
-			links: null,
-			selections: null,
-			posts: null,
+			titleString: '2018',
 		},
 		{
 			title: 2017,
-			job: null,
-			links: null,
-			selections: null,
-			posts: null,
+			titleString: '2017',
 		},
 	]
+
+	yearsList.forEach(year => {
+		year.job = {
+			hired: jobsList.filter(item => dayjs(item.recruited).format('YYYY') === year.titleString)[0] || false,
+			fired: jobsList.filter(item => dayjs(item.dismissal).format('YYYY') === year.titleString)[0] || false,
+			work: jobsList.filter(item => workNow(item, year.titleString))[0] || false,
+		}
+
+		year.links = linksList.filter(item => dayjs(item.create).format('YYYY') === year.titleString)
+		year.selections = selectionsList.filter(item => dayjs(item.create).format('YYYY') === year.titleString)
+		year.posts = postsList.filter(item => dayjs(item.create).format('YYYY') === year.titleString)
+	})
 
 	return (
 		<MainWrapper
