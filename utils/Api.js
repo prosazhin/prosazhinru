b56628d2e553c26bb87179d97472938674ec5892
prosazhin <prosazhin@yandex.ru>
2021-05-17 16:52:48 +0300

    add env

--- utils/Api.js
+++ utils/Api.js
@@ -1,16 +1,13 @@
 import { createClient } from 'contentful'
 
-const SPACE_ID = 'hz8shqd8njrb'
-const ACCESS_TOKEN = 'iFdc0FqW5DDf-LYA6e-oLiiMoucIQdcRIqC_dKHbdQE'
-
 
 
 export default class API {
 
     constructor() {
         this.client = createClient({
-            space: SPACE_ID,
-            accessToken: ACCESS_TOKEN,
+            space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+            accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
         })
     }
 
