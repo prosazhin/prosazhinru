890a6225e702b677c1af0d6cc1e302b1c0f7e383
prosazhin <prosazhin@yandex.ru>
2021-06-02 23:56:33 +0300

    fix

--- pages/links/[tag].js
+++ pages/links/[tag].js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import style from './styles.module.scss'
-import { dataTabs } from '../../utils/tabs'
+import { dataTabs } from '../../utils/tabs.js'
 
 import {
 	MainWrapper,
