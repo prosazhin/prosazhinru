2686a662139cb585fa7c31eed040f36e228e6413
prosazhin <prosazhin@yandex.ru>
2021-08-07 17:10:08 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     webpack5: true,
     eslint: {
-        dirs: ['components', 'context', 'methods', 'pages', 'scss', 'utils'],
+        dirs: ['components', 'context', 'methods', 'pages', 'utils'],
     },
     images: {
         domains: ['images.ctfassets.net'],
