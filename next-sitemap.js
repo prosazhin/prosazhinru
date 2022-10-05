47ab2cf0d19d0562e493ed4aaac0859a95f4b310
prosazhin <prosazhin@yandex.ru>
2022-10-05 17:31:28 +0300

    Update

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,18 +1,16 @@
 module.exports = {
-    siteUrl: 'https://prosazhin.ru',
-    generateRobotsTxt: true,
-    exclude: ['/sitemap.xml'],
-    robotsTxtOptions: {
-        policies: [
-            {
-                userAgent: '*',
-                host: 'http://prosazhin.ru',
-                allow: '/',
-                disallow: '/_next',
-            },
-        ],
-        additionalSitemaps: [
-            'https://prosazhin.ru/sitemap.xml'
-        ],
-    },
-}
+  siteUrl: 'https://prosazhin.ru',
+  generateRobotsTxt: true,
+  exclude: ['/sitemap.xml'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        host: 'http://prosazhin.ru',
+        allow: '/',
+        disallow: '/_next',
+      },
+    ],
+    additionalSitemaps: ['https://prosazhin.ru/sitemap.xml'],
+  },
+};
