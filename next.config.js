32c9160f3b1a0c0b3ab70a6ff9ee05d51a145b52
prosazhin <prosazhin@yandex.ru>
2023-04-03 13:58:05 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
     dirs: ['components', 'context', 'methods', 'pages', 'utils'],
+  },
+  sassOptions: {
+    fiber: false,
+    includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['images.ctfassets.net'],
