825e0032b8d860fa844ee414752158a9923ca90f
prosazhin <prosazhin@yandex.ru>
2023-10-15 19:56:42 +0200

    Fixes sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,24 +1,110 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const PROD = process.env.NEXT_PUBLIC_NODE_ENV === 'production';
+const SITE_URL = PROD ? 'https://prosazhin.ru' : 'http://localhost:8080';
+
+const data = [
+  {
+    path: '/',
+    priority: 1.0,
+  },
+  {
+    path: '/en',
+    priority: 1.0,
+  },
+  {
+    path: '/career',
+    priority: 0.9,
+  },
+  {
+    path: '/en/career',
+    priority: 0.9,
+  },
+  {
+    path: '/competencies',
+    priority: 0.8,
+  },
+  {
+    path: '/en/competencies',
+    priority: 0.8,
+  },
+  {
+    path: '/projects',
+    priority: 0.8,
+  },
+  {
+    path: '/en/projects',
+    priority: 0.8,
+  },
+  {
+    path: '/posts',
+    priority: 0.7,
+  },
+  {
+    path: '/links',
+    priority: 0.7,
+  },
+  {
+    path: '/links?tag=design',
+    priority: 0.7,
+  },
+  {
+    path: '/links?tag=frontend',
+    priority: 0.7,
+  },
+  {
+    path: '/links?tag=tooling',
+    priority: 0.7,
+  },
+  {
+    path: '/links?tag=figma',
+    priority: 0.7,
+  },
+  {
+    path: '/links?tag=dev',
+    priority: 0.7,
+  },
+  {
+    path: '/compilations',
+    priority: 0.7,
+  },
+  {
+    path: '/compilations?tag=design',
+    priority: 0.7,
+  },
+  {
+    path: '/compilations?tag=frontend',
+    priority: 0.7,
+  },
+  {
+    path: '/compilations?tag=tooling',
+    priority: 0.7,
+  },
+  {
+    path: '/compilations?tag=figma',
+    priority: 0.7,
+  },
+  {
+    path: '/compilations?tag=dev',
+    priority: 0.7,
+  },
+];
+
 module.exports = {
-  siteUrl: 'https://prosazhin.ru',
+  siteUrl: SITE_URL,
   generateRobotsTxt: true,
-  transform: async (config, path) => {
-    let newPath = path;
+  additionalPaths: async () => {
+    const result = [];
 
-    if (path.startsWith('/ru')) {
-      newPath = path.replace('/ru', '');
+    data.forEach((item) => {
+      result.push({
+        loc: item.path,
+        changefreq: 'daily',
+        priority: item.priority,
+        lastmod: new Date().toISOString(),
+      });
+    });
 
-      if (path === '/ru') {
-        newPath = '/';
-      }
-    }
-
-    return {
-      loc: newPath,
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-      alternateRefs: config.alternateRefs ?? [],
-    };
+    return result;
   },
 };
