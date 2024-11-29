7fc0539b5841a88c35f8e10be9420e91bd7e589e
prosazhin <prosazhin@yandex.ru>
2024-11-29 15:35:23 +0300

    fix: not-found page fixes

--- src/app/[locale]/[...not-found]/page.jsx
+++ src/app/[locale]/[...not-found]/page.jsx
@@ -1,5 +1,21 @@
 import { notFound } from 'next/navigation';
 
-export default function NotFoundCatchAll() {
+import { initTranslations } from '@/i18n';
+import getMetadata from '@/utils/get-metadata';
+
+function NotFoundCatchAll() {
   notFound();
 }
+
+export async function generateMetadata({ params: { locale } }) {
+  const { t } = await initTranslations(locale);
+
+  return getMetadata({
+    locale,
+    title: `${t('pages.notFound.title')} | ${t('metaTitle')}`,
+    description: t('metaDescription'),
+    pathname: t('pages.index.pathname'),
+  });
+}
+
+export default NotFoundCatchAll;
