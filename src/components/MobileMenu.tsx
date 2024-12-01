2681f5f1dc1e9691005fab863c2c22fdf0fee917
prosazhin <prosazhin@yandex.ru>
2024-12-01 19:11:20 +0300

    fix: small fixes

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,13 +1,23 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
+
 import { Bars2Icon } from '@heroicons/react/24/outline';
+import { useEffect } from 'react';
 
 import { Button, useDialog } from '@pbcomponents/react';
 
 import Nav from '@/components/Nav';
 
 const MobileMenu = () => {
+  const pathname = usePathname();
   const { showDialog } = useDialog();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.scrollTo({ top: 0 });
+    }
+  }, [pathname]);
 
   return (
     <>
